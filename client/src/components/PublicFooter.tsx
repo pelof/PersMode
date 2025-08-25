9472d57f9726be07c5c533e6b793d32c8904efec
Per Elofsson <elofper@gmail.com>
2025-08-25 11:02:13 +0200

    responsive icons

--- client/src/components/PublicFooter.tsx
+++ client/src/components/PublicFooter.tsx
@@ -9,22 +9,22 @@ export function PublicFooter() {
   return (
     <>
       
-      <ul className="footer-icons grid grid-cols-1 justify-start m-4">
-        <li className="flex items-center justify-center flex-row w-full my-2">
+      <ul className="footer-icons grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-start m-4 md:justify-items-center">
+        <li className="flex items-center flex-row my-2">
           <FaGlobeAfrica className="text-4xl"/>
           <div className="w-full ml-2">Gratis frakt och returer</div>
         </li>
-        <li className="flex items-center justify-center flex-row w-full my-2">
+        <li className="flex items-center flex-row my-2">
                       <FaShippingFast  className="text-4xl"/>
 
           <div className="w-full ml-2">Expressfrakt</div>
         </li>
-        <li className="flex items-center justify-center flex-row w-full my-2">
+        <li className="flex items-center flex-row my-2">
                       <FaShieldAlt  className="text-4xl"/>
 
           <div className="w-full ml-2">Säkra betalningar</div>
         </li>
-        <li className="flex items-center justify-center flex-row w-full my-2">
+        <li className="flex items-center flex-row my-2">
                       <FaRegSmile  className="text-4xl"/>
 
           <div className="w-full ml-2">Nyheter varje dag</div>
