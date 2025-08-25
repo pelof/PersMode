54c3f53874890c7b1603538599d8552cdd37b6d5
Per Elofsson <elofper@gmail.com>
2025-08-25 15:30:33 +0200

    product grid first draft

---
+++ client/src/components/CardGrid.tsx
@@ -0,0 +1,16 @@
+import { ProductCard } from "./ProductCard";
+
+export function CardGrid() {
+    return (
+        <section className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 mt-4">
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        </section>
+    )
+}
