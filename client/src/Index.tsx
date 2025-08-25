1e444d360ab94a7678684f27cca9371a1f1ba9a4
Per Elofsson <elofper@gmail.com>
2025-08-25 14:40:45 +0200

    hero finish first draft

--- client/src/Index.tsx
+++ client/src/Index.tsx
@@ -1,8 +1,7 @@
+import { Hero } from "./components/Hero";
+
 export function Index() {
     return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            <p>This is the public home page of the application.</p>
-        </div>
+        <Hero/>
     );
 }
