e89c7c20e7d1db455a845f9b455c7abfb29b5b03
Per Elofsson <elofper@gmail.com>
2025-08-28 16:07:18 +0200

    hero fungerande

--- client/src/routes/Index.tsx
+++ client/src/routes/Index.tsx
@@ -6,10 +6,16 @@ import { useProducts } from "../api/products";
 export function Index() {
     const { data, isLoading } = useProducts({});
 
+    
     if (isLoading) return <p>Laddar...</p> 
+    if (!data || data.length === 0) return <p>Inga produkter hittades</p>;
+    
+    //För random Hero från db
+    const randomIndex = Math.floor(Math.random() * data.length)
+    const heroProduct = data[randomIndex];
     return (
         <>
-        <Hero/>
+        <Hero product={heroProduct}/>
         <Spots products={data?.slice(8,11) ?? []}/>
         <CardGrid products={data?.slice(0,8) ?? []}/>
         </>
